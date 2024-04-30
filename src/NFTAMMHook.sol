4658195cffe5a00c03c63e93936e29862f201ae5
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2024-04-30 19:44:30 -0400

    updating imports

--- src/NFTAMMHook.sol
+++ src/NFTAMMHook.sol
@@ -5,6 +5,18 @@ pragma solidity ^0.8.0;
 import {ERC1155} from "solmate/tokens/ERC1155.sol";
 import {BaseHook} from "v4-periphery/BaseHook.sol";
 
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+
+import {PoolId, PoolIdLibrary} from "v4-core/types/PoolId.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+
 contract NFTAMMHook is ERC1155, BaseHook {
     constructor(){
 
