2c2fa00665d10c15f88d4f0b334c8595a96bc7f9
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2024-04-30 19:43:43 -0400

    adding standards

---
+++ src/NFTAMMHook.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+
+import {ERC1155} from "solmate/tokens/ERC1155.sol";
+import {BaseHook} from "v4-periphery/BaseHook.sol";
+
+contract NFTAMMHook is ERC1155, BaseHook {
+    constructor(){
+
+    }
+}
