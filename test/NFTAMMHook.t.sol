f744d9fa10592c8777d2ee5bb67fa1adba241904
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2024-05-04 22:17:28 -0400

    adding setup

--- test/NFTAMMHook.t.sol
+++ test/NFTAMMHook.t.sol
@@ -5,6 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
 import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+import {MockERC721} from "solmate/test/utils/mocks/MockERC721.sol";
 
 import {PoolManager} from "v4-core/PoolManager.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
@@ -19,10 +20,56 @@ import {HookMiner} from "./utils/HookMiner.sol";
 
 contract PointsHookTest is Test, Deployers {
     using CurrencyLibrary for Currency;
-    
+
+
+    MockERC20 token;
+    MockERC721 collection;
+
+    Currency ethCurrency = Currency.wrap(0);
+    Currency tokenCurrency;
+
+    NFTAMMHook hook;
+
 
     function setUp() public {
+        deployFreshManagerAndRouters();
 
+        token = new MockERC20();
+        tokenCurrency = Currency.wrap(address(token));
+
+        token.mint(address(this), 1000 ether);
+        token.mint(address(1), 1000 ether);
+
+        uint160 flags = uint160(
+            Hooks.AFTER_ADD_LIQUIDITY_FLAG | Hooks.AFTER_SWAP_FLAG
+        );
+        (, bytes32 salt) = HookMiner.find(
+            address(this),
+            flags,
+            0,
+            type(NFTAMMHook).creationCode,
+            abi.encode(manager, "Wrapped Token", "TEST_WRAPPED", 18)
+        );
+
+        // Deploy our hook
+        hook = new NFTAMMHook{salt: salt}(
+            manager,
+            "Wrapped Token",
+            "TEST_WRAPPED",
+            18
+        );
+
+        token.approve(address(swapRouter), type(uint256).max);
+        token.approve(address(modifyLiquidityRouter), type(uint256).max);
+
+        (key, ) = initPool(
+            ethCurrency, // Currency 0 = ETH
+            tokenCurrency, // Currency 1 = TOKEN
+            hook, // Hook Contract
+            3000, // Swap Fees
+            SQRT_RATIO_1_1, // Initial Sqrt(P) value = 1
+            ZERO_BYTES // No additional `initData`
+        );
     }
 
 }
