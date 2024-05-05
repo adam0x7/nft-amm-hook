cf9adeb888bf3ce9722363fd9a5f62c9b4c6e184
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2024-05-04 21:53:13 -0400

    updating test

---
+++ test/NFTAMMHook.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+import {Test} from "forge-std/Test.sol";
+
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
+import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
+
+import {PoolManager} from "v4-core/PoolManager.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+
+import {PointsHook} from "../src/PointsHook.sol";
+import {HookMiner} from "./utils/HookMiner.sol";
+
+contract PointsHookTest is Test, Deployers {
+    using CurrencyLibrary for Currency;
+
+}
