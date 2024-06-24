1488bef5dd4632220fa8f705c12b7d23d86f4331
adam0x7 <18746720+adam0x7@users.noreply.github.com>
2024-06-25 00:40:31 +0200

    Update NFTAMMHook.t.sol

--- test/NFTAMMHook.t.sol
+++ test/NFTAMMHook.t.sol
@@ -26,19 +26,6 @@ import {IERC721} from "openzeppelin/interfaces/IERC721.sol";
 import {LiquidityAmounts} from "@uniswap/v4-core/test/utils/LiquidityAmounts.sol";
 
 contract NFTAMMHookTest is Test, Deployers {
-    struct BidOrder {
-        address maker;
-
-        bool immediate;
-
-        uint256 ethValue;
-
-        uint256 bidId;
-
-        int24 bidTick;
-
-        uint256 orderId;
-    }
     using CurrencyLibrary for Currency;
 
     MockERC20 token;
@@ -52,27 +39,16 @@ contract NFTAMMHookTest is Test, Deployers {
     address public maker = address(1);
 
     PoolId id;
-
-
-
+    
     function setUp() public {
         deployFreshManagerAndRouters();
 
-        uint256[] memory tokenIds = new uint256[](5);
-        tokenIds[0] = 0;
-        tokenIds[1] = 1;
-        tokenIds[2] = 2;
-        tokenIds[3] = 3;
-        tokenIds[4] = 4;
-
-        for(uint256 i = 0; i < tokenIds.length; i++) {
+        for (uint256 i = 0; i < 5; i++) {
             vm.prank(maker);
-            collection.safeMint(address(maker), tokenIds[i]);
+            collection.safeMint(address(maker), i);
         }
 
-        uint160 flags = uint160(
-            Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_SWAP_FLAG
-        );
+        uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG | Hooks.BEFORE_SWAP_FLAG);
         (, bytes32 salt) = HookMiner.find(
             address(this),
             flags,
@@ -81,7 +57,6 @@ contract NFTAMMHookTest is Test, Deployers {
             abi.encode(manager, "Wrapped Token", "TEST_WRAPPED", type(uint256).max, address(collection), 18)
         );
 
-        // Deploy our hook
         hook = new NFTAMMHook{salt: salt}(
             manager,
             "Wrapped Token",
@@ -91,31 +66,30 @@ contract NFTAMMHookTest is Test, Deployers {
             18
         );
 
-        vm.deal(address(hook), 10000000000000000000000000000000000000000000);
+        vm.deal(address(hook), 1000000000000);
 
+        address[] memory approveAddresses = [
+                        address(swapRouter),
+                        address(modifyLiquidityRouter),
+                        address(manager),
+                        address(hook)
+            ];
 
-        vm.prank(address(hook));
-        hook.approve(address(swapRouter), type(uint256).max);
-        vm.prank(address(hook));
-        hook.approve(address(modifyLiquidityRouter), type(uint256).max);
-        vm.prank(address(hook));
-        hook.approve(address(manager), type(uint256).max);
-        vm.prank(address(hook));
-        hook.approve(address(hook), type(uint256).max);
-
-
+        for (uint256 i = 0; i < approveAddresses.length; i++) {
+            vm.prank(address(hook));
+            hook.approve(approveAddresses[i], type(uint256).max);
+        }
 
         tokenCurrency = Currency.wrap(address(hook));
 
         (key, id) = initPool(
-            ethCurrency, // Currency 0 = ETH
-            tokenCurrency, // Currency 1 = TOKEN
-            hook, // Hook Contract
-            3000, // Swap Fees
-            SQRT_RATIO_1_1, // Initial Sqrt(P) value = 1
-            ZERO_BYTES // No additional `initData`
+            ethCurrency,
+            tokenCurrency,
+            hook,
+            3000,
+            SQRT_RATIO_1_1,
+            ZERO_BYTES
         );
-
     }
 
 
@@ -123,122 +97,91 @@ contract NFTAMMHookTest is Test, Deployers {
     function testMarketOrderCreation() public {
         int24 startingBuyTick = -60;
         int24 startingSellTick = 60;
-        uint256 delta = 10;
-        uint256 fee = 20;
-        uint256 maxNumOfNFTsToBuy = 5;
-
         uint256 balanceBefore = collection.balanceOf(address(maker));
 
-        //redundant tokenIds
         uint256[] memory tokenIds = new uint256[](5);
-        tokenIds[0] = 0;
-        tokenIds[1] = 1;
-        tokenIds[2] = 2;
-        tokenIds[3] = 3;
-        tokenIds[4] = 4;
+        for (uint256 i = 0; i < 5; i++) {
+            tokenIds[i] = i;
+        }
 
         vm.deal(maker, 10e18);
-
-        vm.prank(maker);
+        vm.startPrank(maker);
         collection.setApprovalForAll(address(hook), true);
-        vm.prank(maker);
-        hook.marketMake{ value: 5}(
+        hook.marketMake{value: 5}(
             address(collection),
             startingBuyTick,
             startingSellTick,
             tokenIds,
-            delta,
-            fee,
-            maxNumOfNFTsToBuy
+            10, // delta
+            20, // fee
+            5   // maxNumOfNFTsToBuy
         );
+        vm.stopPrank();
 
-       assert(hook.makerBalances(maker) > 0);
+        assert(hook.makerBalances(maker) > 0);
 
+        (uint160 currentSqrtPrice,,, uint24 swapFee) = manager.getSlot0(id);
         uint160 sqrtRatioAX96 = TickMath.getSqrtRatioAtTick(startingBuyTick);
         uint160 sqrtRatioBX96 = TickMath.getSqrtRatioAtTick(startingSellTick);
 
-        uint256 amount0 = 5; // eth deposited from market making order
-        uint256 amount1 = hook.makerBalances(maker); // amount of wrapped tokens for NFT measured in eth. calculated in market maker order
-
-        uint160 currentSqrtPrice;
-        int24 currentTick;
-        uint24 swapFee;
-        (currentSqrtPrice, currentTick, fee, swapFee) = manager.getSlot0(id);
-
         uint128 liquidityToAdd = LiquidityAmounts.getLiquidityForAmounts(
             currentSqrtPrice,
-            sqrtRatioAX96,       // Lower tick sqrt price
-            sqrtRatioBX96,       // Upper tick sqrt price
-            amount0,             // ETH amount
-            amount1              // wNFT amount (in ETH equivalent)
+            sqrtRatioAX96,
+            sqrtRatioBX96,
+            5,  // amount0: ETH deposited from market making order
+            hook.makerBalances(maker)  // amount1: wNFT amount in ETH equivalent
         );
 
         vm.deal(address(this), 2000000000);
-
         vm.prank(address(hook));
-
-        uint256 liquidityToAddUint256 = uint256(liquidityToAdd);
-        int256 liquidityToAddInt256 = int256(liquidityToAddUint256);
-
         modifyLiquidityRouter.modifyLiquidity{value: liquidityToAdd}(
             key,
             IPoolManager.ModifyLiquidityParams({
-                tickLower: -60,
-                tickUpper: 60,
-                liquidityDelta: liquidityToAddInt256
+                tickLower: startingBuyTick,
+                tickUpper: startingSellTick,
+                liquidityDelta: int256(uint256(liquidityToAdd))
             }),
-            "" // empty bytes
+            ""
         );
 
-    assert(balanceBefore - tokenIds.length == collection.balanceOf(address(maker)));
+        assert(balanceBefore - tokenIds.length == collection.balanceOf(address(maker)));
     }
 
-   function testNFTPurchase() public {
+    function testNFTPurchase() public {
         address trader = address(0x2);
-       vm.deal(maker, 20);
-       vm.deal(trader, 10 ether);
+        uint256 nftId = 0;
+
+        vm.deal(maker, 20);
+        vm.deal(trader, 10 ether);
         uint256 initialTraderBalance = trader.balance;
         uint256 initialMakerBalance = maker.balance;
 
+        (uint160 currentSqrtPrice,,,) = manager.getSlot0(id);
+        uint160 sqrtRatioAX96 = TickMath.getSqrtRatioAtTick(0);
+        uint160 sqrtRatioBX96 = TickMath.getSqrtRatioAtTick(6);
 
-       //adding liquidity
-        uint256 nftId = 0;
-        uint256 fee = 20;
-       uint160 currentSqrtPrice;
-       int24 currentTick;
-       uint24 swapFee;
-       (currentSqrtPrice, currentTick, fee, swapFee) = manager.getSlot0(id);
-       uint160 sqrtRatioAX96 = TickMath.getSqrtRatioAtTick(0);
-       uint160 sqrtRatioBX96 = TickMath.getSqrtRatioAtTick(6);
+        uint256 amount0 = 5 ether;
+        uint256 amount1 = hook.makerBalances(maker) * 10e18;
 
-       uint256 amount0 = 5 ether; // eth deposited from market making order
-       uint256 amount1 = hook.makerBalances(maker) * 10e18; // amount of wrapped tokens for NFT measured in eth. calculated in market maker order
-
-
-   uint128 liquidityToAdd = LiquidityAmounts.getLiquidityForAmounts(
-           currentSqrtPrice,
-           sqrtRatioAX96,       // Lower tick sqrt price
-           sqrtRatioBX96,       // Upper tick sqrt price
-           amount0,             // ETH amount
-           amount1              // wNFT amount (in ETH equivalent)
-       );
-
+        uint128 liquidityToAdd = LiquidityAmounts.getLiquidityForAmounts(
+            currentSqrtPrice,
+            sqrtRatioAX96,
+            sqrtRatioBX96,
+            amount0,
+            amount1
+        );
 
         vm.deal(address(hook), 1000000000000000000000000000000000000000 ether);
-       vm.prank(address(hook));
-
-       uint256 liquidityToAddUint256 = uint256(liquidityToAdd);
-       int256 liquidityToAddInt256 = int256(liquidityToAddUint256);
-
-       modifyLiquidityRouter.modifyLiquidity{value: liquidityToAdd}(
-           key,
-           IPoolManager.ModifyLiquidityParams({
-               tickLower: TickMath.minUsableTick(60),
-               tickUpper: TickMath.maxUsableTick(60),
-               liquidityDelta: liquidityToAddInt256
-           }),
-           "" // empty bytes
-       );
+        vm.prank(address(hook));
+        modifyLiquidityRouter.modifyLiquidity{value: liquidityToAdd}(
+            key,
+            IPoolManager.ModifyLiquidityParams({
+                tickLower: TickMath.minUsableTick(60),
+                tickUpper: TickMath.maxUsableTick(60),
+                liquidityDelta: int256(uint256(liquidityToAdd))
+            }),
+            ""
+        );
 
         IPoolManager.SwapParams memory params = IPoolManager.SwapParams({
             zeroForOne: true,
@@ -252,25 +195,20 @@ contract NFTAMMHookTest is Test, Deployers {
             currencyAlreadySent: false
         });
 
-       uint256[] memory tokenIds = new uint256[](1);
-       tokenIds[0] = nftId;
+        uint256[] memory tokenIds = new uint256[](1);
+        tokenIds[0] = nftId;
 
+        vm.startPrank(maker);
+        collection.setApprovalForAll(address(hook), true);
+        hook.marketMake{value: 5 ether}(address(collection), 0, 60, tokenIds, 10, 20, 1);
+        vm.stopPrank();
 
-
-       vm.prank(maker);
-       collection.setApprovalForAll(address(hook), true);
-       vm.prank(maker);
-       hook.marketMake{value: 5 ether}(address(collection), 0, 60, tokenIds, 10, 20, 1);
-
-
-
-       vm.prank(address(trader));
+        vm.prank(address(trader));
         bytes memory order = hook.createBuyBidOrder{value: 2 ether}(1, nftId, maker);
 
         vm.prank(address(hook));
         swapRouter.swap{value: 2 ether}(key, params, testSettings, order);
 
-        // Assertions for buying the NFT
         assertEq(collection.ownerOf(nftId), trader, "NFT should be transferred to the trader");
         assertGt(maker.balance, initialMakerBalance, "Maker should receive the Ether payment");
         assertLt(trader.balance, initialTraderBalance, "Trader's balance should be decreased by the Ether amount");
